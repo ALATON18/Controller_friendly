f57de8fb31be10296abca7969d68fc339eb657bd
ALATON18 <littlexdoesminecraft@gmail.com>
2026-06-28 22:30:54 +0100

    Add compatibility mod id constants

---
+++ src/main/java/com/alaton18/controllerfriendly/client/compat/CompatIds.java
@@ -0,0 +1,16 @@
+package com.alaton18.controllerfriendly.client.compat;
+
+public final class CompatIds {
+	public static final String JEI = "jei";
+	public static final String EMI = "emi";
+	public static final String AE2 = "ae2";
+	public static final String REFINED_STORAGE = "refinedstorage";
+	public static final String CREATE = "create";
+	public static final String SOPHISTICATED_STORAGE = "sophisticatedstorage";
+	public static final String JOURNEYMAP = "journeymap";
+	public static final String FTB_CHUNKS = "ftbchunks";
+	public static final String FTB_QUESTS = "ftbquests";
+
+	private CompatIds() {
+	}
+}
